c5ec52a16dd2865349189c03bb4537599cd3122b
montanus-wecib <amontanus@wcpss.net>
2026-05-17 14:40:44 -0400

    experiementing with nested loop

--- for-loop/program.py
+++ for-loop/program.py
@@ -1,4 +1,7 @@
 total = 0
-for i in range(1, 6):
-    total = total + i
+for i in range(1, 3):
+    for j in range(1, 3):
+        if i == j:
+            continue
+        total = total + i + j
     print(total, i)
