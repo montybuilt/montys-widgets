3bcaff0f1965e4fdb2665b51b05f4ffc28651c6d
montanus-wecib <amontanus@wcpss.net>
2026-05-17 14:46:00 -0400

    experiementing with while loop

--- for-loop/program.py
+++ for-loop/program.py
@@ -1,7 +1,13 @@
-total = 0
-for i in range(1, 3):
-    for j in range(1, 3):
-        if i == j:
-            continue
-        total = total + i + j
-    print(total, i)
+# total = 0
+# for i in range(1, 3):
+#     for j in range(1, 3):
+#         if i == j:
+#             continue
+#         total = total + i + j
+#     print(total, i)
+
+x = 4
+
+while x > 0:
+    print(x)
+    x = x - 1
