e03f1ee0a1b47938f86857d30bd94045abca0d30
montanus-wecib <amontanus@wcpss.net>
2026-05-17 15:45:39 -0400

    testing comments

--- for-loop/program-04-nested.py
+++ for-loop/program-04-nested.py
@@ -1,3 +1,5 @@
+# Nested loops are loops inside of loops. They allow you to perform more complex iterations, such as iterating over a grid or a matrix.
+
 for row in range(1, 4):
     for col in range(1, 4):
         print(row * col)
