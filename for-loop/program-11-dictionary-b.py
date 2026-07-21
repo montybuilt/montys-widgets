fda529817c2082f58aabe16f2040a8c2279b305f
montanus-wecib <amontanus@wcpss.net>
2026-07-21 11:24:12 -0400

    added 3 examples for dict loops

---
+++ for-loop/program-11-dictionary-b.py
@@ -0,0 +1,4 @@
+scores = {"Ava": 91, "Leo": 87}
+
+for score in scores.values():
+    print(score)
---
+++ for-loop/program-11-dictionary-c.py
@@ -0,0 +1,4 @@
+scores = {"Ava": 91, "Leo": 87}
+
+for name, score in scores.items():
+    print(name, score)
