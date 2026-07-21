76354b1f3917e6038894631980ffedd88c911a62
montanus-wecib <amontanus@wcpss.net>
2026-07-21 12:24:49 -0400

    added another nested loop

---
+++ for-loop/program-04-nested-b.py
@@ -0,0 +1,4 @@
+grid = [[1, 2], [3, 4]]
+for row in grid:
+    for value in row:
+        print(value)
--- for-loop/program-05-break-continue.py
+++ for-loop/program-05-break-continue.py
@@ -1,6 +1,8 @@
 for i in range(1, 8):
+
     if i == 3:
         continue
     if i == 6:
         break
+    
     print(i)
